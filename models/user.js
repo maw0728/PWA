6cf4ca56f43abab82d8aa8ae189bf7fce0218ec7
maw0728 <maw0728@naver.com>
2020-11-09 08:13:20 +0900

    0.0.1

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        userId: {
+          type: Sequelize.STRING(10),
+          allowNull: true,
+          unique: true,
+        },
+        password: {
+          type: Sequelize.STRING(20),
+          allowNull: true,
+        },
+        nick: {
+          type: Sequelize.STRING(20),
+          allowNull: true,
+          unique: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "User",
+        tableName: "users",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+};
