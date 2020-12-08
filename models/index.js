08c03f6589ce7b7a34c5a0497550b93247d11daf
jihyeon <osye1128@naver.com>
2020-12-08 16:08:40 +0900

    commit

--- models/index.js
+++ models/index.js
@@ -4,11 +4,7 @@ const config = require("../config/config")[env];
 const User = require("./user");
 const Board = require("./board");
 const Movie = require("./movie");
-<<<<<<< HEAD
 const Point = require("./point");
-=======
-const Comment= require('./comment');
->>>>>>> feature/board_comment
 
 const db = {};
 const sequelize = new Sequelize(
@@ -23,7 +19,6 @@ db.sequelize = sequelize;
 db.User = User;
 db.Board = Board;
 db.Movie = Movie;
-<<<<<<< HEAD
 db.Point = Point;
 
 User.init(sequelize);
@@ -35,16 +30,5 @@ User.associate(db);
 Board.associate(db);
 Movie.associate(db);
 Point.associate(db);
-=======
-db.Comment=Comment;
-User.init(sequelize);
-Board.init(sequelize);
-Movie.init(sequelize);
-Comment.init(sequelize);
-User.associate(db);
-Board.associate(db);
-Movie.associate(db);
-Comment.associate(db);
->>>>>>> feature/board_comment
 
 module.exports = db;
